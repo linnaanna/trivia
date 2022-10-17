01e62b740ec75d6ca5096042eb3700dfadc4d4cc
linnaanna <linna.anna@gmail.com>
2022-10-17 11:24:46 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [results, setResults] = useState([]);
+  const [request, setRequest] = useState('');
+  
+  
+  const getData = () => {
+    setResults(fetch('https://opentdb.com/api.php?amount=1')
+    .then(response => response.json())
+    .then(resdata => {setResults(resdata.results);}))
+    setRequest('?');
+  }
+
+    const getQuestion = () =>{
+
+    results.map(getQ);
+
+    function getQ (item) {
+      setRequest(item.question)
+    } 
+  }
+
+  const getAnswer = () => {
+    
+    results.map(getA);
+
+    function getA (item) {
+      setRequest(item.correct_answer)
+    }
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+           <p> {request} </p>
+      <button class="button" onClick={getData}>Refresh</button>
+      <button class="button" onClick={getQuestion}>Question</button>
+      <button class="button" onClick={getAnswer}>Answer</button>
+      <footer class="footer">©Anna Linna 2022</footer>
     </div>
+    
   );
-}
+  } 
 
 export default App;
